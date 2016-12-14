9c6a9cabfdfdec89e071aaa11ba3a1632b079a25
Robin la Fleur <lafleur.robin@gmail.com>
2016-12-14 11:13:12 +0100

    changes

--- app/src/main/java/se/mah/ae5710/iotapapp/MainActivity.java
+++ app/src/main/java/se/mah/ae5710/iotapapp/MainActivity.java
@@ -4,12 +4,16 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
@@ -36,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 for (BluetoothDevice device : pairedDevices) {
                     mDevice = device;
                 }
-                Log.i("hej hopp", "hej hopp");
                 mConnectThread = new ConnectThread(mDevice);
                 mConnectThread.start();
             }
@@ -71,5 +74,72 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) { }
         }
     }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+
+            }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int begin = 0; int bytes = 0;
+            while (true) {
+                try {
+                    bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
+                    for(int i = begin; i < bytes; i++) {
+                        if(buffer[i] == "#".getBytes()[0]) {
+                        mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
+                        begin = i + 1;
+                            if(i == bytes - 1) {
+                                bytes = 0; begin = 0;
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+        public void write(byte[] bytes) {
+            try { mmOutStream.write(bytes);
+            } catch (IOException e) {
+
+            }
+        }
+
+        public void cancel() {
+            try { mmSocket.close();
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            byte[] writeBuf = (byte[]) msg.obj;
+            int begin = (int)msg.arg1;
+            int end = (int)msg.arg2;
+            switch(msg.what) {
+                case 1: String writeMessage = new String(writeBuf);
+                    writeMessage = writeMessage.substring(begin, end);
+                    break;
+            }
+        }
+    };
 
 }
